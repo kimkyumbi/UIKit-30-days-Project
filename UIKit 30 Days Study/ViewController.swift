0e43969036a33dbab9542973192bdc010c86fad8
kimkyumbi <kimgyeombi@naver.com>
2023-12-15 13:41:59 +0900

    Initial Commit

---
+++ UIKit 30 Days Study/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  UIKit 30 Days Study
+//
+//  Created by Mac on 12/15/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
