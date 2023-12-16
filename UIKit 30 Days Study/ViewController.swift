7d5801c08efc519937ca3865021a9894fbd1a13f
kimkyumbi <kimgyeombi@naver.com>
2023-12-16 22:18:20 +0900

    UILabel

--- UIKit 30 Days Study/ViewController.swift
+++ UIKit 30 Days Study/ViewController.swift
@@ -6,14 +6,41 @@
 //
 
 import UIKit
+import SwiftUI
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        let firstLabel = UILabel()
+        
+        firstLabel.text = "유킷유캔두잇"
+        
+        view.addSubview(firstLabel)
+        
+        let safeArea = view.safeAreaLayoutGuide
+        let leadingConstraint = firstLabel.leadingAnchor.constraint(equalTo: safeArea.leadingAnchor, constant: 16)
+        
+        firstLabel.translatesAutoresizingMaskIntoConstraints = false
+        
+        leadingConstraint.isActive = true
     }
-
-
 }
 
+struct ViewControllerRepresentable: UIViewControllerRepresentable {
+    typealias UIViewControllerType = ViewController
+    
+    func makeUIViewController(context: Context) -> ViewController {
+        return ViewController()
+    }
+    
+    func updateUIViewController(_ uiViewController: ViewController, context: Context) {
+    }
+}
+
+@available(iOS 13.0.0, *)
+struct ViewPreview: PreviewProvider {
+    static var previews: some View {
+        ViewControllerRepresentable()
+    }
+}
