c929fcbafb3b9f67e07e7a4cdc1d2a454e415122
kimkyumbi <kimgyeombi@naver.com>
2024-01-01 18:45:28 +0900

    update

--- UIKit 30 Days Study/ViewController.swift
+++ UIKit 30 Days Study/ViewController.swift
@@ -3,40 +3,28 @@ import SwiftUI
 
 class ViewController: UIViewController {
     
-    @IBOutlet weak var myImageView: UIImageView!
-    @IBOutlet weak var label: UILabel!
-
+    lazy var button: UIButton = {
+        let button = UIButton()
+        button.setTitle("버튼입니다", for: .normal)
+        button.backgroundColor = .lightGray
+        button.translatesAutoresizingMaskIntoConstraints = false
+        return button
+    }()
+    
+    lazy var label: UILabel = {
+        let label = UILabel()
+        label.text = "2024년 파이팅"
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.textAlignment = .center
+        return label
+    }()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        let firstLabel = UILabel()
         
-        firstLabel.text = "유킷유캔두잇"
-        
-        view.addSubview(firstLabel)
-        
-        let safeArea = view.safeAreaLayoutGuide
-        let leadingConstraint = firstLabel.leadingAnchor.constraint(equalTo: safeArea.leadingAnchor, constant: 16)
-        
-        firstLabel.translatesAutoresizingMaskIntoConstraints = false
-        
-        leadingConstraint.isActive = true
+        view.addSubview(button)
+        button.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 20).isActive = true
+        button.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -20).isActive = true
+        button.topAnchor.constraint(equalTo: view.topAnchor, constant: 300).isActive = true
     }
 }
-
-//struct ViewControllerRepresentable: UIViewControllerRepresentable {
-//    typealias UIViewControllerType = ViewController
-//    
-//    func makeUIViewController(context: Context) -> ViewController {
-//        return ViewController()
-//    }
-//    
-//    func updateUIViewController(_ uiViewController: ViewController, context: Context) {
-//    }
-//}
-//
-//@available(iOS 13.0.0, *)
-//struct ViewPreview: PreviewProvider {
-//    static var previews: some View {
-//        ViewControllerRepresentable()
-//    }
-//}
